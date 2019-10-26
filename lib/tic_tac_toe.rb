0574069d8fa596edfa7479791800f7a368d8efdc
Otto Cedeno <otto@ottostacos.com>
2019-10-26 13:08:17 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -55,7 +55,7 @@ def turn(board)
   index = input_to_index(user_input)
 
   if valid_move?(board, index)
-    move(board, index, player = "X")
+    move(board, index, current_player(board))
     display_board(board)
   else
     turn(board)
